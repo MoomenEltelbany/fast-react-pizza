d566d120a8561097951824d1412aef96fc4dd5cf
Moomen Eltelbany <73857848+MoomenEltelbany@users.noreply.github.com>
2025-08-16 18:11:08 +0300

    Change the layout of the cart component

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,10 +1,17 @@
 import { useLoaderData } from "react-router-dom";
 import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
 
 function Menu() {
     const menu = useLoaderData();
 
-    return <ul></ul>;
+    return (
+        <ul>
+            {menu.map((pizza) => (
+                <MenuItem pizza={pizza} key={pizza.id} />
+            ))}
+        </ul>
+    );
 }
 
 // eslint-disable-next-line react-refresh/only-export-components
