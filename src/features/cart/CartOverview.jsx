af6c48235bc19f2d45636cd39571e4d579117f66
Moomen Eltelbany <73857848+MoomenEltelbany@users.noreply.github.com>
2025-08-26 01:01:33 +0300

    Create the Button and LinkButton components

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import LinkButton from "../../ui/LinkButton";
 
 function CartOverview() {
     return (
@@ -8,7 +8,7 @@ function CartOverview() {
                 <span>$23.45</span>
             </p>
 
-            <Link to="/cart">Open cart &rarr;</Link>
+            <LinkButton to="/cart">Open cart &rarr;</LinkButton>
         </div>
     );
 }
--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,6 @@
 function UserName() {
-    return <p className="hidden text-sm font-semibold md:block">Moomen</p>;
+    return <p className="hidden text-sm font-semibold">Moomen</p>;
+    // return <p className="hidden text-sm font-semibold md:block">Moomen</p>;
 }
 
 export default UserName;
